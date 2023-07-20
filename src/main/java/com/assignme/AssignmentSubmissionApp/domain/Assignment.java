6d50dc3868a54eff001721e6bbcf55b02e2b9532
Thomas <cuongnguyen14122001@gmail.com>
2023-07-20 21:56:26 +0700

    create db & objects

---
+++ src/main/java/com/assignme/AssignmentSubmissionApp/domain/Assignment.java
@@ -0,0 +1,66 @@
+package com.assignme.AssignmentSubmissionApp.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+// create table call assignment (based on class name)
+public class Assignment {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String status;
+    private String githubUrl;
+    private String branch;
+    private String codeReviewUrl;
+    @ManyToOne(optional = false)
+    private User user;
+//    private User assignedTo;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getGithubUrl() {
+        return githubUrl;
+    }
+
+    public void setGithubUrl(String githubUrl) {
+        this.githubUrl = githubUrl;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public String getCodeReviewUrl() {
+        return codeReviewUrl;
+    }
+
+    public void setCodeReviewUrl(String codeReviewUrl) {
+        this.codeReviewUrl = codeReviewUrl;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ src/main/java/com/assignme/AssignmentSubmissionApp/domain/User.java
@@ -0,0 +1,53 @@
+package com.assignme.AssignmentSubmissionApp.domain;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate cohortStartDate;
+    private String name;
+    private String password;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getCohortStartDate() {
+        return cohortStartDate;
+    }
+
+    public void setCohortStartDate(LocalDate cohortStartDate) {
+        this.cohortStartDate = cohortStartDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
